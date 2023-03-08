0dbcd5f839a524d90792a3ce2af3e980ad785f2f
Francisco Jesus <francisco.adan@qastusoft.com>
2023-03-08 12:49:37 +0100

    Creados value objects para representar un curso

---
+++ src/BoundedContext/Courses/Domain/ValueObjects/CourseId.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Core\BoundedContext\Course\Domain\ValueObject;
+
+use Core\Shared\Domain\ValueObjects\StringValueObject;
+
+final class CourseId extends StringValueObject {}
---
+++ src/BoundedContext/Courses/Domain/ValueObjects/CourseName.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Core\BoundedContext\Course\Domain\ValueObject;
+
+use Core\Shared\Domain\ValueObjects\StringValueObject;
+
+final class CourseName extends StringValueObject {}
