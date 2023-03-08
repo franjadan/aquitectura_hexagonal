da3a1aca5bf21c251ebcee4168661858f6309b2b
Francisco Jesus <francisco.adan@qastusoft.com>
2023-03-08 13:28:07 +0100

    Creado caso de uso listar cursos

---
+++ src/BoundedContext/Courses/Application/Actions/ListCourses.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Core\BoundedContext\Course\Application\Actions;
+
+use Core\BoundedContext\Course\Application\Responses\CoursesResponse;
+use Core\BoundedContext\Course\Domain\CourseRepository;
+
+final class ListCourses {
+    public function __construct(private CourseRepository $repository) {}
+
+    
+    public function __invoke(): CoursesResponse {
+        $courses = $this->repository->list();
+        return CoursesResponse::fromCourses($courses);
+    }
+}
