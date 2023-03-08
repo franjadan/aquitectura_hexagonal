f54447980e96ad4f48c2e9d97071090e83a685cb
Francisco Jesus <francisco.adan@qastusoft.com>
2023-03-08 12:44:30 +0100

    Creada clase abstracta para excepciones de eloquent y definida la infraestructura shared

---
+++ src/BoundedContext/Shared/Infrastructure/InfrastructureException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Shared\Infrastructure;
+
+use Exception;
+
+abstract class InfrastructureException extends Exception {}
+
---
+++ src/BoundedContext/Shared/Infrastructure/Persistence/Eloquent/EloquentException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Shared\Infrastructure\Persistence\Eloquent;
+
+use Core\Shared\Infrastructure\InfrastructureException;
+
+abstract class EloquentException extends InfrastructureException {}
+
