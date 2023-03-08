f3a83ade5859847e34790571ca909c81644efd9e
Francisco Jesus <francisco.adan@qastusoft.com>
2023-03-08 13:11:59 +0100

    Creado CourseResponse

--- src/BoundedContext/Courses/Domain/CourseRepository.php
+++ src/BoundedContext/Courses/Domain/CourseRepository.php
@@ -5,5 +5,11 @@ namespace Core\BoundedContext\Courses\Domain;
 use Core\BoundedContext\Course\Domain\ValueObject\CourseId;
 
 interface CourseRepository {
-    
+    public function list(): Courses;
+
+    public function save(Course $course): void;
+
+    public function find(CourseId $id): ?Course;
+
+    public function delete(CourseId $id): void;
 }
