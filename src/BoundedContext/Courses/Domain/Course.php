3b107eb5bf4dd38624cf5dfaa0113d1bec6d41fc
Francisco Jesus <francisco.adan@qastusoft.com>
2023-03-08 13:00:26 +0100

    Creada representación curso a nivel de dominio

---
+++ src/BoundedContext/Courses/Domain/Course.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core\BoundedContext\Courses\Domain;
+
+use Core\BoundedContext\Course\Domain\ValueObject\CourseId;
+use Core\BoundedContext\Course\Domain\ValueObject\CourseName;
+
+class Course {
+    public function __construct(private CourseId $id, private CourseName $name) {}
+
+    /**
+     * Para pasar datos primitivos (por ejemplo de eloquent) a value objects
+     */
+    public static function fromPrimitives(string $id, string $name): self {
+        return new self(
+            new CourseId($id),
+            new CourseName($name),
+        );
+    }
+
+    //Crear una nueva instancia
+    public static function create(CourseId $id, CourseName $name): self {
+        return new self($id, $name);
+    }
+
+    public function id(): CourseId {
+        return $this->id;
+    }
+
+    public function name(): CourseName {
+        return $this->name;
+    }
+}
