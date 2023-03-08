8c9b915a6dc5c2c2f5464bc72a09c87cdca95672
Francisco Jesus <francisco.adan@qastusoft.com>
2023-03-08 13:07:04 +0100

    Creada interfaz repositorio para cursos

---
+++ src/BoundedContext/Courses/Domain/CourseRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Core\BoundedContext\Courses\Domain;
+
+use Core\BoundedContext\Course\Domain\ValueObject\CourseId;
+
+interface CourseRepository {
+    
+}
---
+++ src/BoundedContext/Courses/Domain/Courses.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Core\BoundedContext\Courses\Domain;
+
+use Core\Shared\Domain\Collection;
+
+final class Courses extends Collection {}
